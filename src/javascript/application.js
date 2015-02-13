415bad58372c05dc565c051d97dee06136585e6c
MrBrownser <castany.adria@gmail.com>
2015-02-13 16:31:30 +0100

    Already obtaining track data, including id

--- src/javascript/application.js
+++ src/javascript/application.js
@@ -8,62 +8,56 @@
 // The field filter tag:new can be used in album searches to retrieve only albums released in the last two weeks. The field filter tag:hipster can be used in album searches to retrieve only albums with the lowest 10% popularity.
 // Other possible field filters, depending on object types being searched, include genre, upc, and isrc. For example, q=damian+genre:reggae-pop&type=artist.
 
-var SEARCH_URL = "https://api.spotify.com/v1/search/?type=track&"
+var SEARCH_URL = "https://api.spotify.com/v1/search/?type=track&q="
 var GET_MUSIC_URL = "https://api.spotify.com/v1/tracks/{id}"
 
-var artist_name = "Nas";
-
-var prueba = function give_me_artist() {
+var prueba = function give_me_track(track_name, callback) {
+ 	var url = SEARCH_URL + track_name;
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', encodeURI(SEARCH_URL + 'query=' + artist_name));
-	xhr.onload = function() {
+	xhr.open('GET', encodeURI(url));
+	xhr.send();
+	xhr.onload = function(event) {
 	    if (xhr.status === 200) {
-	    	console.log(xhr.response);
-	        // alert('User\'s name is ' + xhr.responseText);
+			var response = JSON.parse(event.target.response);
+			callback(response);
 	    }
 	    else {
 	        alert('Request failed.  Returned status of ' + xhr.status);
 	    }
 	};
-	xhr.send();
 };
 
+function show_track_info(artist, track) {
+	// debugger;
+	document.querySelector(".title").innerHTML = track;
+	document.querySelector(".author").innerHTML = artist;
+}
+
+function getSongId(response) {
+	var track_info = response.tracks.items[0];
+	var artist = track_info.artists[0].name;
+	var track = track_info.name;
+	// PUT ARTIST NAME ON BROWSER
+	// PUT TRACK NAME ON BROWSER
+	show_track_info(artist, track);
+	
+	var track_id = track_info.id;
+
+
+	console.log("Artist: " + artist);
+	console.log("Song found: " + track);
+	console.log("ID: " + track_id);
+}
+
+prueba("The Message", getSongId);
 
 
 // spotifySongSearch( "run like hell", getSongId );
 
-function spotifySongSearch(track, callback) {
- var url = "https://api.spotify.com/v1/search/?type=track&q=" + track;
- req.open("GET", url);  
- req.send(); 
- req.onload = function(event) {
-   var response = JSON.parse(event.target.response);
-   callback(response);
- };
-}
 
-
-
-function getSongId(response){
-    getSongById(response.tracks.items[0].id, songReceivedHandler);
-}
-
-
-
-prueba();
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function getSongId(response){
+//     getSongById(response.tracks.items[0].id, songReceivedHandler);
+// }
 
 // play_btn = document.getElementByClassName("btn-play");
 
